942235a85848edadd3e89858148e78dd7ef537ea
Toe Naing Win <142083872+RusisCki@users.noreply.github.com>
2025-05-12 20:41:42 +0630

    Add files via upload

---
+++ yjscript.js
@@ -0,0 +1,249 @@
+let Songs = ["YNW_Melly_-__Suicidal.mp3", 
+    "YNW_Melly_-_223s_ft._9lokknine.mp3",
+    "YNW_Melly_-_772_Love__Lyrics_(256k).mp3",
+    "YNW_Melly_-_Bang_Bang_[Official_Audio](256k).mp3","Ynw_Melly_-_Blue_Balenciagas__Music_Video_(256k).mp3",
+    "YNW_Melly_-_City_Girls(128k).mp3",
+    "YNW_Melly_-_Dangerously_In_Love(256k).mp3" ,
+    "YNW_Melly_-_Mama_Cry(256k).mp3",
+    "YNW_Melly_-_Mind_of_Melvin__feat._Lil_Uzi_Vert_(256k).mp3","YNW_Melly_-_Murder_On_My_Mind(256k).mp3",
+    "YNW_Melly_-_No_Heart(256k).mp3",
+    "YNW_Melly_ft._Kanye_West_-_Mixed_Personalities__Official_Music_Video_(256k).mp3","Juice_WRLD___benny_blanco_-_Graduation__Lyrics_(256k).mp3","Juice_WRLD_-_All_Girls_Are_The_Same__Lyrics_(256k).mp3","Juice_WRLD_-_Already_Dead__Official_Audio_(256k).mp3",
+    "Juice_WRLD_-_Black___White(256k).mp3" ,
+    "Juice_WRLD_-_Candles__Lyrics_(256k).mp3",
+    "Juice_WRLD_-_Cheese_and_Dope_Freestyle(256k).mp3" ,
+    "Juice_WRLD_-_Dark_Inside__Music_Video_(256k).mp3",
+    "Juice_WRLD_-_Devils_Heart,_Gone__AI___Prod._by_Eto_s_Eyes_(256k).mp3","Juice_WRLD_-_Empty__Lyrics_(256k).mp3",
+    "Juice_WRLD_-_End_Of_The_Road__Official_Audio_(256k).mp3",
+    "Juice_WRLD_-_Face_2_Face(256k).mp3",
+    "Juice_WRLD_-_Fast(256k).mp3" ,
+    "Juice_WRLD_-_Glo’d_Up(256k).mp3",
+    "Juice_WRLD_-_Hate_Me__without_ellie_goulding_(256k).mp3",
+    "Juice_WRLD_-_Hear_Me_Calling(256k).mp3",
+    "Juice_WRLD_-_I_ll_Be_Fine__Official_Audio_(256k).mp3","Juice_WRLD_-_Kinda_Lonely__Unreleased__[Prod.RockyRoadz_X_80Root](256k).mp3","Juice_WRLD_-_Lean_Wit_Me__Lyrics_(256k).mp3",
+    "Juice_WRLD_-_Legends__Music_Video_Tribute_(256k).mp3",
+    "Juice_WRLD_-_Lucid_Dreams(256k).mp3",
+    "Juice_WRLD_-_No_Good(256k).mp3",
+    "Juice_WRLD_-_Righteous__Official_Video_(256k).mp3",
+    "Juice_WRLD_-_Robbery__Lyrics_(256k).mp3",
+    "Juice_WRLD_-_Scared_Of_Love___with_instrumental_by_Ghost_Loft___Official_Audio_(256k).mp3",
+    "Juice_WRLD_-_Stay_High__Official_Audio_(256k).mp3",
+    "Juice_WRLD-__Conversations__Official_Music_Video_(256k).mp3",
+    "Juice_WRLD,_Eminem___benny_blanco_-_Lace_It__Official_Audio_(256k).mp3",
+    "Juice_WRLD,_Lil_Uzi_Vert_-_Wasted__Lyrics_(256k).mp3",
+];
+
+function one() {
+    alert("To Play YNW_Melly_-__Suicidal Press OK!");
+    playSongs(0);
+}
+function two() {
+    alert("To Play YNW_Melly_-_223s_ft._9lokknine Press OK!");
+    playSongs(1);
+}
+function three() {
+    alert("To Play YNW_Melly_-_772_Love Press OK!");
+    playSongs(2);
+}
+function four() {
+    alert("To Play YNW_Melly_-_Bang_Bang Press OK!");
+    playSongs(3);
+}
+function five() {
+    alert("To Play Ynw_Melly_-_Blue_Balenciagas Press OK!");
+    playSongs(4);
+}
+function six() {
+    alert("To Play YNW_Melly_-_City_Girls Press OK!");
+    playSongs(5);
+}
+function seven() {
+    alert("To Play YNW_Melly_-_Dangerously_In_Love Press OK!");
+    playSongs(6);
+}
+function eight() {
+    alert("To Play YNW_Melly_-_Mama_Cry Press OK!");
+    playSongs(7);
+}
+function nine() {
+    alert("To Play YNW_Melly_-_Mind_of_Melvin__feat._Lil_Uzi_Vert Press OK!");
+    playSongs(8);
+}
+function ten() {
+    alert("To Play YNW_Melly_-_Murder_On_My_Mind Press OK!");
+    playSongs(9);
+}
+function oneone() {
+    alert("To Play YNW_Melly_-_No_Heart Press OK!");
+    playSongs(10);
+}
+function onetwo() {
+    alert("To Play YNW_Melly_ft._Kanye_West_-_Mixed_Personalities Press OK!");
+    playSongs(11);
+}
+function onethree() {
+    alert("To Play Juice_WRLD___benny_blanco_-_Graduation Press OK!");
+    playSongs(12);
+}
+function onefour() {
+    alert("To Play Juice_WRLD_-_All_Girls_Are_The_Same Press OK!");
+    playSongs(13);
+}
+function onefive() {
+    alert("To Play Juice_WRLD_-_Already_Dead Press OK!");
+    playSongs(14);
+}
+function onesix() {
+    alert("To Play Juice_WRLD_-_Black___White Press OK!");
+    playSongs(15);
+}
+function oneseven() {
+    alert("To Play Juice_WRLD_-_Candles Press OK!");
+    playSongs(16);
+}
+function oneeight() {
+    alert("To Play Juice_WRLD_-_Cheese_and_Dope_Freestyle Press OK!");
+    playSongs(17);
+}
+function onenine() {
+    alert("To Play Juice_WRLD_-_Dark_Inside__Music_Video Press OK!");
+    playSongs(18);
+}
+function twozero() {
+    alert("To Play Juice_WRLD_-_Devils_Heart Press OK!");
+    playSongs(19);
+}
+function twoone() {
+    alert("To Play Juice_WRLD_-_Empty Press OK!");
+    playSongs(20);
+}
+function twotwo() {
+    alert("To Play Juice_WRLD_-_End_Of_The_Road__Official_Audio Press OK!");
+    playSongs(21);
+}
+function twothree() {
+    alert("To Play Juice_WRLD_-_Face_2_Face Press OK!");
+    playSongs(22);
+}
+function twofour() {
+    alert("To Play Juice_WRLD_-_Fast Press OK!");
+    playSongs(23);
+}
+function twofive() {
+    alert("To Play Juice_WRLD_-_Glo’d_Up Press OK!");
+    playSongs(24);
+}
+function twosix() {
+    alert("To Play Juice_WRLD_-_Hate_Me__without_ellie_goulding Press OK!");
+    playSongs(25);
+}
+function twoseven() {
+    alert("To Play Juice_WRLD_-_Hear_Me_Calling Press OK!");
+    playSongs(26);
+}
+function twoeight() {
+    alert("To Play Juice_WRLD_-_I_ll_Be_Fine__Official_Audio Press OK!");
+    playSongs(27);
+}
+function twonine() {
+    alert("To Play Juice_WRLD_-_Kinda_Lonely__Unreleased Press OK!");
+    playSongs(28);
+}
+function threezero() {
+    alert("To Play Juice_WRLD_-_Lean_Wit_Me Press OK!");
+    playSongs(29);
+}
+function threeone() {
+    alert("To Play Juice_WRLD_-_Legends__Music_Video_Tribute Press OK!");
+    playSongs(30);
+}
+function threetwo() {
+    alert("To Play Juice_WRLD_-_Lucid_Dreams Press OK!");
+    playSongs(31);
+}
+function threethree() {
+    alert("To Play Juice_WRLD_-_No_Good Press OK!");
+    playSongs(32);
+}
+function threefour() {
+    alert("To Play Juice_WRLD_-_Righteous Press OK!");
+    playSongs(33);
+}
+function threefive() {
+    alert("To Play Juice_WRLD_-_Robbery Press OK!");
+    playSongs(34);
+}
+function threesix() {
+    alert("To Play Juice_WRLD_-_Scared_Of_Love Press OK!");
+    playSongs(35);
+}
+function threeseven() {
+    alert("To Play Juice_WRLD_-_Stay_High Press OK!");
+    playSongs(36);
+}
+function threeeight() {
+    alert("To Play Juice_WRLD-__Conversation Press OK!");
+    playSongs(37);
+}
+function threenine() {
+    alert("To Play Juice_WRLD,_Eminem___benny_blanco Press OK!");
+    playSongs(38);
+}
+function fourzero() {
+    alert("To Play Juice_WRLD,_Lil_Uzi_Vert_-_Wasted Press OK!");
+    playSongs(39);
+}
+function random()
+{
+    playSongs(getnumber()) ;
+    document.getElementByIdElement("ran").reset();
+}
+var pauseid = document.getElementById("pauseid");
+var playid = document.getElementById("playid");
+function playSongs(index) {
+    let colid = index ;
+    if (index < Songs.length) {
+        let nowsong = new Audio(Songs[index]);
+        nowsong.play();
+
+         //All are nameless function
+       plus.addEventListener("click" ,function ()
+      {
+        nowsong.currentTime +=30;
+        }) ;
+        minus.addEventListener("click",function()
+    {
+        nowsong.currentTime -= 30 ;
+    });
+
+        pauseid.addEventListener("click", function() {
+            nowsong.pause();
+        });
+        const playListener = function () {
+            nowsong.play();
+        };
+        playid.addEventListener("click", playListener);
+        document.getElementById(colid).style.backgroundColor=getRandomColor();
+        document.getElementById('songname').innerHTML = "Now Playing: " + Songs[index];
+        nowsong.addEventListener('ended', function() {
+            playSongs(index + 1);
+            colid+1 ;
+            playid.removeEventListener("click", playListener);
+        },{ once: true });
+    }
+}
+
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function getnumber ()
+{
+    let num = Math.floor(Math.random()*(40-0)+1) ;
+    return num ;
+}
